ec6b0a0ee362c77de775a52cbad8813df71ae712
Aman Sayeed <amansayeed@gmail.com>
2022-09-27 10:16:51 +0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'snailelockers_ENG' );
+define( 'DB_NAME', '##' );
 
 /** MySQL database username */
-define( 'DB_USER', 'snailelockers_ENG' );
+define( 'DB_USER', '## );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'hi&]Nc-!-X(O' );
+define( 'DB_PASSWORD', '##' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
